ddad57ac2465e9771b8914d19978fb057e14c09a
kpobrien4 <48386395+kpobrien4@users.noreply.github.com>
2019-06-01 16:53:15 -0400

    Update array_crud.rb
    Arrays were created

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,43 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+[1,2,3,4]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+array = [1,2,3,4]
+array<<element
 end
 
 def add_element_to_start_of_array(array, element)
-  
+array = [1,2,3,4]
+array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+array = [1,2,"arrays!"]
+array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+array = ["wow",1]
+array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+array = ["hey","ham","am"]
+array[2]
+
 end
 
 def retrieve_first_element_from_array(array)
-  
+array = ["wow","am"]
+array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array = ["I","arrays!"]
+  array[-1]
 end
