0bb6f12bad5418d5c4641541ac8e2df7b4179c81
Kevin O'Brien <kpobrien@buffalo.edu>
2019-06-01 20:51:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -33,9 +33,11 @@ array[2]
 end
 
 def retrieve_first_element_from_array(array)
-
+array = ["I","am"]
+array[0]
 end
 
 def retrieve_last_element_from_array(array)
-
+  array = ["I","am"]
+  array[-1]
 end
