4144111c145659f4bb1d4f823e582c16c8fe533c
Kevin O'Brien <kpobrien@buffalo.edu>
2019-06-01 20:44:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,14 +17,17 @@ array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-
+array = []
+array.pop
 end
 
 def remove_element_from_start_of_array(array)
-
+array = []
+array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
+array[]
 
 end
 
